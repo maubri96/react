9550a1f7e64acc2b7c6b47791dda6147991a68bf
maubri96 <rauzav96@outlook.es>
2025-10-29 12:56:02 -0600

    REACT5

--- src/componentes/hooks/useFetchData.js
+++ src/componentes/hooks/useFetchData.js
@@ -1,26 +1,28 @@
 import { useState, useEffect } from "react"
+import { fetchdata } from "../helpers/fetchData"
+
+//const getData = async () => {
+//const {data, isLoading} = await fetchdata(endPoint)
+//setData(data)
+//setIsLoading(data)
+//} --> luego se coloca en el useEffect "getData()"
+
+
 export const useFetchData = (endPoint) => {
 
-    const [data, setData] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+   const [data, setData] = useState([])
+   const [isLoading, setIsLoading] = useState(true)
 
-    const fetchdata = async () => {
-        try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/${endPoint}`)
-            const data = await response.json()
-            setData(data)
-            setIsLoading(false)
-        } catch (error) {
-            console.error(error)
-        }
-    }
+   useEffect(() => {
+      fetchdata(endPoint)
+         .then(res => {
+            setData(res.data)
+            setIsLoading(res.isLoading)
+         })
+   }, [endPoint])
 
-    useEffect(() => { 
-        fetchdata() 
-    }, [endPoint])
-
- return {
-    data,
-    isLoading
- }
+   return {
+      data,
+      isLoading
+   }
 }
